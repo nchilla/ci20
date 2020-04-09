c880b9c884aa35efefe0bc8e7570ef1897291c13
Nico <jnico.chilla@gmail.com>
2020-04-08 21:04:23 -0700

    sonnets draft 2

--- collection/js/function.js
+++ collection/js/function.js
@@ -1,19 +1,24 @@
 var root=document.documentElement;
-colH=100;
+var colH=100;
 var senlngth=3;
+var allsonnets;
 
-
+//transfer json to variable and add to dom--------------------------------------------------------
 function buildBody(data){
   allsonnets=data;
   var currentson=0;
+  var curline=1;
   for(var i=0;i<allsonnets.length;i++){
     if(currentson==allsonnets[i].sonnet){
     }else{
       currentson++;
-      d3.select('#maintext').append('span').classed('sonnet'+currentson,true).html('❤');
+      currentline=1;
+      d3.select('#maintext').append('span').classed('sonnet',true).classed('sonnet'+currentson,true).append('span').classed('heart',true).html('❤'+currentson+' ');
     }
-    d3.select('#maintext').select('.sonnet'+currentson).append('span').html(' '+allsonnets[i].line+' ');
+    d3.select('#maintext').select('.sonnet'+currentson).append('span').html(' '+allsonnets[i].line+' ').classed('line'+currentline,true);
+    currentline++;
   }
+  poemExp();
   /*
   d3.select('#maintext')
   .transition()
@@ -26,7 +31,7 @@ function buildBody(data){
 
 
 
-
+//top bar--------------------------------------------------------
 var topbar=document.querySelector('#topbar');
 topbar.addEventListener("mouseenter", function(event){
   var currentH=$('#topbar').height();
@@ -46,14 +51,44 @@ topbar.addEventListener("mouseenter", function(event){
 topbar.addEventListener("mouseleave", function(event){
   var currentH=$('#topbar').height();
   var winH=window.innerHeight;
-  var slidetime=Math.abs(100-currentH);
+  var slidetime=Math.abs(colH-currentH);
   d3.select('#topbar')
   .transition()
   .duration(slidetime)
   .ease(d3.easePolyIn)
   .style('height',colH+'px');
 });
+//poem expand interface--------------------------------------------------------
+function poemExp(){
+  sonnets=document.querySelectorAll('.sonnet')
+  for (var i=0; i<sonnets.length;i++){
+      sonnets[i].addEventListener("mousedown", function(event){
+      d3.select('#maintext').selectAll('#selected').attr('id','');
+      var selected=d3.select(event.srcElement.parentNode);
+      if (selected.attr('id')=='maintext'){
+        selected=d3.select(event.srcElement);
+      }
+      selected.attr('id','selected');
+      scrollControl('smooth');
+      // document.querySelector('#selected').scrollIntoView({block:'end',behavior: 'smooth'});
+    });
+  }
 
+
+}//end of poemexp
+
+function scrollControl(behave){
+  behave
+  if(d3.select('#selected')._groups[0][0]!==null){
+    var windowRel=document.querySelector('#selected').getBoundingClientRect().y-(colH+50);
+    window.scroll({top:window.scrollY+windowRel,behavior:behave});
+  }
+}
+
+
+function resizing(){
+  scrollControl('auto');
+}
 /*
 function scaleTests(){
 
@@ -65,6 +100,5 @@ function scaleTests(){
     root.style.setProperty('--textwidth', typewidth+'vw')
 }
 
-scaleTests();
-window.addEventListener("resize",scaleTests);
-*/
+scaleTests();*/
+window.addEventListener("resize",resizing);
